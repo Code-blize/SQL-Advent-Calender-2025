969c3253fae67fc6f10db88c2c066f42b445fc5a
Obasi-Uzoma Blessing <118572591+Code-blize@users.noreply.github.com>
2026-01-02 02:10:26 +0100

    Create Elf Vacation Status.sql

---
+++ Day 5/Elf Vacation Status.sql
@@ -0,0 +1,16 @@
+-- Difficulty: Medium
+
+
+/*
+  Some elves took time off after the holiday rush, but not everyone has returned to work. 
+List all elves by name, showing their return date. 
+If they have not returned from vacation, list their return date as "Still resting".
+*/
+
+-- Here's my SQL query for question:
+SELECT 
+    e.elf_name,
+    COALESCE(v.return_date, 'Still resting') AS return_status
+FROM elves e
+LEFT JOIN vacations v
+    ON e.elf_id = v.elf_id;
