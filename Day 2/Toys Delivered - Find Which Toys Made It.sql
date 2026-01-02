0d04bd4685f0faa1c3a14add41696cd530ce8a2c
Obasi-Uzoma Blessing <118572591+Code-blize@users.noreply.github.com>
2026-01-02 02:17:07 +0100

    Fix comment formatting in SQL file
    Updated comments to use SQL comment syntax.

--- Day 2/Toys Delivered - Find Which Toys Made It.sql
+++ Day 2/Toys Delivered - Find Which Toys Made It.sql
@@ -1,8 +1,11 @@
-Difficulty: Medium
+-- Difficulty: Medium
 
-Santa wants to analyze which toys that were produced in his workshop have already been delivered to children. You are given two tables on toy production and toy delivery — can you return the toy ID and toy name of the toys that have been delivered?
+/*
+Santa wants to analyze which toys that were produced in his workshop have already been delivered to children. 
+You are given two tables on toy production and toy delivery — can you return the toy ID and toy name of the toys that have been delivered?
+*/
 
-Here's my SQL query for question 2:
+-- Here's my SQL query for question 2:
 
 SELECT tp.toy_id, tp.toy_name
 FROM toy_production AS tp
